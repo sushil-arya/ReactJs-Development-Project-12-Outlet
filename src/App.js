f2211a8e2e9ba094ac10c591b509c278b4d370f7
sushil-arya <sushilarya888@gmail.com>
2024-03-24 15:35:33 +0530

    Final Done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import React from "react";
 
-function App() {
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import PageNotFound from "./pages/PageNotFound";
+import Layout from "./pages/Layout";
+
+export default function App() {
   return (
-    <div>
-      <h1>Hello..</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element = { <Layout /> }>
+            <Route index element={<Home />}></Route>
+            <Route path="login" element={<Login />}></Route>
+            <Route path="register" element={<Register />}></Route>
+            <Route path="*" element={<PageNotFound />}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
-
-export {App};
---
+++ src/pages/Login.js
@@ -0,0 +1,13 @@
+//import {SomeNamedImport}from SomeLibrary
+import React, { Component } from "react";
+
+export default class Login extends Component {
+  //1. Property
+  //2. Constructor
+
+  //3. Render Method
+  render() {
+    //Every method return something
+    return <main>Login Main</main>;
+  }
+}
